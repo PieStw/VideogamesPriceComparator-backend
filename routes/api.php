dacf1377c49e640038d2a194cc162cebcf32def3
PieStw <pie.stalteri01@gmail.com>
2025-04-01 22:40:28 +0200

    fix 2 la vendetta

--- routes/api.php
+++ routes/api.php
@@ -5,7 +5,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 
-Route::get("videogames", [VideogameController::class, 'index']);
-Route::get("videogames/{videogame}", [VideogameController::class, 'show']);
 Route::get("videogames/bestseller", [VideogameController::class, 'bestseller'])
     ->name("videogames.bestseller");
+Route::get("videogames", [VideogameController::class, 'index']);
+Route::get("videogames/{videogame}", [VideogameController::class, 'show']);
